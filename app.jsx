55a1887fff8be758cca617d0cc4a5abf1152b277
Luis García Estrades <luisgarciaestrades@gmail.com>
2019-10-21 21:44:09 +0200

    Real initial commit

---
+++ app.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const Title = ({ msg = "Hello Parcel" }) =>
+    <h1>{msg}</h1>
+
+export default () =>
+    <><Title msg="Hello World" /></>
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import App from "./app"
+
+const mountingNode = document.querySelector("#app")
+ReactDOM.render(App(), mountingNode)
